8f989582897ab6c4f23a2c606d8053af6064301c
DhruvMalhotra01 <malhotradhruv112004@gmail.com>
2026-07-22 09:15:55 +0530

    21-07-2026

--- hello.py
+++ hello.py
@@ -1,1 +1,55 @@
-print("Hello World")
+# class Student:
+
+#     def __init__(self,name):
+#         self.name=name
+
+#     def display(self):
+#         print(self.name)
+
+# s=Student("Dhruv")
+# s.display()
+
+
+# nums = [1, 2, 3, 4, 5]
+
+# i = 0
+# while i < len(nums):
+#     print(nums[i])
+#     i += 1
+
+
+# for num in nums:
+#     print(num)
+
+# for i in range(len(nums)):
+#     print(i , nums[i])
+
+# for i , num in enumerate(nums):
+#     print(i , num)
+
+# square = []
+# for i in range(5):
+#     square.append(i**2)
+
+# square = [i**2 for i in range(5)]
+
+# print(square)
+
+a = 10
+b  = 20
+# temp = a
+# a = b 
+# b = temp
+
+# a, b = b , a
+
+# print(a,b)
+
+x = 4
+
+# if x == 1 or x == 2 or x == 3:
+
+if x in [1,3,2]:
+    print("find the number")
+else:
+    print("not found")
